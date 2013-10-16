4205700d53eaae0bbe6ffaed4750c15034db868c
EliCash82 <AlmostAnderson@gmail.com>
2013-10-16 04:19:56 +0100

    changed production.rb so that assets not compressed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,10 @@ SampleApp::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+  ###
+  # Compress JavaScripts and CSS // 4:17am changing to see if I can get bootstrap to work proper
+  config.assets.compress = false
+  ###
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
