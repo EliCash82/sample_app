5918bf2caa574515fcaf419b5abea79daf24582d
EliCash82 <AlmostAnderson@gmail.com>
2013-11-14 03:36:19 -0500

    Added initial research to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ SampleApp::Application.routes.draw do
   match '/signup', to: 'users#new'
   match '/signin', to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
-  match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
+  match '/license', to: 'static_pages#license'
   match '/contact', to: 'static_pages#contact'  
  
   
